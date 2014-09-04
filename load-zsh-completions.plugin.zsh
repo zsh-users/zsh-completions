58fa86e56742dcdcb782d0c11217221fc54fb1b6
Joe Block <jpb@unixorn.net>
2014-09-03 19:52:07 -0700

    Clarify description

--- load-zsh-completions.plugin.zsh
+++ load-zsh-completions.plugin.zsh
@@ -1,4 +1,5 @@
-# Simple plugin script to make loading the zsh-completions repo into $fpath
+# Simple plugin script to automate loading the zsh-completions repo into
+# $fpath for antigen users
 
 COMPLETIONS_D="$(dirname $0)/src"
-fpath=(${COMPLETIONS_D} $fpath)
+fpath=("${COMPLETIONS_D}" $fpath)
