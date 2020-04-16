176bad44de27089884c79c9c256e47e26f3dd4e0
Grigorii Horos <horosgrisa@gmail.com>
2020-04-16 17:19:34 +0300

    Make plugin compatible with zsh plugin standard

--- zsh-completions.plugin.zsh
+++ zsh-completions.plugin.zsh
@@ -1,1 +1,8 @@
-fpath+="${0:h}/src"
+# Standarized $0 handling, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
